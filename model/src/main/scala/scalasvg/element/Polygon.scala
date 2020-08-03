fc596852c518441f051ad10b0724025f20d10f9c
Kirill Volkov <neborodulin@gmail.com>
2020-08-03 22:51:35 +0300

    feat: create Polygon element

---
+++ model/src/main/scala/scalasvg/element/Polygon.scala
@@ -0,0 +1,23 @@
+package scalasvg.element
+
+import scalasvg.attribute.generic.{Core, Styling, ConditionalProcessing}
+import scalasvg.element.category.Category
+
+/**
+  * The <polygon> element defines a closed shape consisting of a set of connected straight line segments. 
+  * The last point is connected to the first point.
+  * @param points This attribute defines the list of points (pairs of x,y absolute coordinates) 
+  * required to draw the polygon.
+  * @param pathLength This attribute lets specify the total length for the path, in user units.
+  */
+final case class Polygon(points: List[(String,String)], pathLength: Number)(attributes: Seq[Ploygon.Attribute])
+    extends Element.Empty
+    with Category.BasicShape
+
+object Ploygon {
+
+  /**
+    * @todo: Extend with all attributes of categories Global Event, Graphical Event, Presentation, Aria
+    */
+  type Attribute = Core.Id | Core.Tabindex | Styling.Class | Styling.Style | ConditionalProcessing.SystemLanguage
+}

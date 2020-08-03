6cabb9355e96b02bc011b720f7db7834f498089e
Kirill Volkov <neborodulin@gmail.com>
2020-08-03 22:55:35 +0300

    feat: create Polyline element

---
+++ model/src/main/scala/scalasvg/element/Polyline.scala
@@ -0,0 +1,24 @@
+package scalasvg.element
+
+import scalasvg.attribute.generic.{Core, Styling, ConditionalProcessing}
+import scalasvg.element.category.Category
+
+/**
+  * The <polyline> SVG element is an SVG basic shape that creates straight lines connecting several points. 
+  * Typically a polyline is used to create open shapes as the last point doesn't have to be connected 
+  * to the first point. For closed shapes see the <polygon> element.
+  * @param points This attribute defines the list of points (pairs of x,y absolute coordinates) 
+  * required to draw the polyline.
+  * @param pathLength This attribute lets specify the total length for the path, in user units.
+  */
+final case class Polyline(points: List[(String,String)], pathLength: Number)(attributes: Seq[Polyline.Attribute])
+    extends Element.Empty
+    with Category.BasicShape
+
+object Polyline {
+
+  /**
+    * @todo: Extend with all attributes of categories Global Event, Graphical Event, Presentation, Aria
+    */
+  type Attribute = Core.Id | Core.Tabindex | Styling.Class | Styling.Style | ConditionalProcessing.SystemLanguage
+}

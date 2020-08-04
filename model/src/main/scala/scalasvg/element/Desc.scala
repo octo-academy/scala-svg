ac8a0415eca5d92236bf13c1d139155e25b86d21
Kirill Volkov <neborodulin@gmail.com>
2020-08-04 11:57:30 +0300

    chore: add javadoc for Desc element

--- model/src/main/scala/scalasvg/element/Desc.scala
+++ model/src/main/scala/scalasvg/element/Desc.scala
@@ -3,6 +3,9 @@ package scalasvg.element
 import scalasvg.attribute.generic.{Core, Styling}
 import scalasvg.element.category.Category
 
+/**
+  * The <desc> element provides an accessible, long-text description of any SVG container element or graphics element.
+  */
 final case class Desc(attributes: Seq[Desc.Attribute])(content: String)
     extends Element.Container[String](content)
     with Category.Descriptive

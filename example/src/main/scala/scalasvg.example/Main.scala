ec841731e09eacdb5128e46edfc11fba98b5ef7a
Denis_Verkhoturov <Denis_Verkhoturov@epam.com>
2020-08-13 18:16:30 +0300

    feat: Add example project showing usage of dsl

---
+++ example/src/main/scala/scalasvg.example/Main.scala
@@ -0,0 +1,23 @@
+package scalasvg.example
+
+import scalasvg.element.SVG
+import scalasvg.dsl.{svg, circle, desc, plain}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val document: SVG =
+      svg(viewBox = "0 0 100 100") {
+        desc() {
+          plain("A description for the document.")
+        }
+        circle(x = "0", y = "0", r = "5px") {
+          desc() {
+            plain("A multiline description")
+            plain("for the circle.")
+          }
+        }
+      }
+
+    println(document)
+  }
+}

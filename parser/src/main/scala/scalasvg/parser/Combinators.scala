20d1919a8a5126e343d29eedee21843969ed4b0a
Denis Verkhoturov <mod.satyr@gmail.com>
2021-02-06 18:27:08 +0300

    feat: Add primitive parsers and combinators

--- parser/src/main/scala/scalasvg/parser/Combinators.scala
+++ parser/src/main/scala/scalasvg/parser/Combinators.scala
@@ -3,8 +3,11 @@ package scalasvg.parser
 import scalasvg.lang.std.ListInstances.{ given }
 import scalasvg.lang.typeclass.Applicative
 import scalasvg.parser.internal.Parser
+import scalasvg.parser.CharParsers.{ Space, char }
 
 trait Combinators {
+  final def optional[O](parser: Parser[O]): Parser[Option[O]] = parser.map(Some(_)) |+| Applicative[Parser].pure(None)
+
   final def many[O](parser: Parser[O]): Parser[List[O]] =
     some(parser) |+| Applicative[Parser].pure(List[O]())
 
@@ -13,6 +16,22 @@ trait Combinators {
       head <- parser
       tail <- many(parser)
     } yield head :: tail
+
+  final def between[OPEN, CLOSE, A](open: Parser[OPEN], close: Parser[CLOSE])(p: Parser[A]): Parser[A] =
+    open *> p <* close
+
+  final def between[OPEN, A](open: Parser[OPEN])(p: Parser[A]): Parser[A] = between(open, open)(p)
+
+  final def token[A](p: Parser[A]): Parser[A] = between(Space, Space)(p)
+
+  final def parentheses[A](p: Parser[A]): Parser[A] = between(char('('), char(')'))(p)
+
+  final def braces[A](p: Parser[A]): Parser[A] = between(char('{'), char('}'))(p)
+
+  final def brackets[A](p: Parser[A]): Parser[A] = between(char('['), char(']'))(p)
+
+  final def quotes[A](p: Parser[A]): Parser[A] = between(char('"'))(p)
+
 }
 
 object Combinators extends Combinators

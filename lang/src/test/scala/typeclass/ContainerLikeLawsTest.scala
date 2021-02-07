86d84fe8c63c14d302d29ff4bdd133fea69661cf
Nadezhda Balashova <hope-doe@nadezhdas-macbook-pro.local>
2021-02-07 09:28:22 +0300

    test: Add Functor, Monad, Monoid laws and tests

---
+++ lang/src/test/scala/typeclass/ContainerLikeLawsTest.scala
@@ -0,0 +1,15 @@
+package typeclass
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.scalacheck.Checkers
+import typeclass.behavior._
+import scalasvg.lang.typeclass._
+
+class ContainerLikeLawsTest[F[_]]
+  extends AnyWordSpec
+    with Matchers
+    with Checkers
+    with FunctorBehavior[F]
+    with MonadBehavior[F]
+    with MonoidConteinerLikeBehavior[F]

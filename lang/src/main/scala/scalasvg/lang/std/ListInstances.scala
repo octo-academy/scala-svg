b84483ffeb1d18ccdd4e9a3504b6951c3f5d67a4
Nadezhda Balashova <hope-doe@nadezhdas-macbook-pro.local>
2021-02-07 09:28:22 +0300

    test: Add Functor, Monad, Monoid laws and tests

--- lang/src/main/scala/scalasvg/lang/std/ListInstances.scala
+++ lang/src/main/scala/scalasvg/lang/std/ListInstances.scala
@@ -1,6 +1,6 @@
 package scalasvg.lang.std
 
-import scalasvg.lang.typeclass.Monoid
+import scalasvg.lang.typeclass.{ Applicative, Apply, Monad, Monoid, Traverse, Equal }
 
 import scala.annotation.targetName
 
@@ -9,6 +9,36 @@ trait ListInstances {
     val empty: List[A] = Nil
 
     extension (a: List[A]) @targetName("combine") def |+|(b: List[A]): List[A] = a ++ b
+  }
+  
+  given ListMonad: Monad[List] with {
+    def pure[A](a: A): List[A] = List(a)
+
+    extension [A, B](fa: List[A]) {
+      def flatMap(f: A => List[B]): List[B] = fa.foldRight(Monoid[List[B]].empty)((a, acc) => f(a) ++ acc)
+    }
+  }
+
+  given ListTraversable: Traverse[List] with {
+    extension[G[_], A, B] (fa: List[A])(using Applicative[G]) {
+      def traverse(f: A => G[B]): G[List[B]] = fa.foldRight(Applicative[G].pure(Monoid[List[B]].empty)) { (a, acc) =>
+        f(a).map2(acc)(_ :: _)
+      }
+    }
+
+    extension [A, B](fa: List[A]) {
+      def foldLeft(b: B)(f: (B, A) => B): B = fa.foldLeft(b)(f)
+
+      def foldRight(b: B)(f: (A, B) => B): B = fa.foldRight(b)(f)
+    }
+  }
+  
+  given ListEqual[A](using A: Equal[A]): Equal[List[A]] with {
+    def equal(x: List[A], y: List[A]) = {
+      x.size == y.size && {
+        x.zip(y).forall { case (xx, yy) => A.equal(xx, yy) }
+      }
+    }
   }
 }
 
---
+++ lang/src/test/scala/typeclass/behavior/MonoidConteinerLikeBehavior.scala
@@ -0,0 +1,34 @@
+package typeclass.behavior
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Prop.forAll
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.scalacheck.Checkers
+import scalasvg.lang.laws.{IsEqual, MonoidLaws}
+import scalasvg.lang.typeclass.{Equal, Monoid}
+
+trait MonoidConteinerLikeBehavior[F[_]] {
+  self: AnyWordSpec with Matchers with Checkers =>
+  
+  def monoid[A](using Arbitrary[F[A]], Equal[F[A]], Monoid[F[A]]) = {
+    val monoidLaws = MonoidLaws[F[A]]
+    
+    "follow all Monoid laws," which afterWord("include"){
+      "combine right identity" in {
+        check {
+          forAll { (a: F[A]) =>
+            monoidLaws.combineRightIdentity(a).isEqual
+          }
+        }
+      }
+      "combine left identity" in {
+        check {
+          forAll { (a: F[A]) =>
+            monoidLaws.combineLeftIdentity(a).isEqual
+          }
+        }
+      }
+    }
+  }
+}
